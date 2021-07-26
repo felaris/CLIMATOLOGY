e19fac3309a300b1cd2a84cd97893c93ee1f6af0
felaris <felixawortwe14@gmail.com>
2021-07-26 11:06:23 -0500

    ploted the first 2 graphs

--- snippet01/trials.py
+++ snippet01/trials.py
@@ -22,3 +22,11 @@ filename = Path('pressure_evaporation_with_time_and_lon_lat.nc')
 
 data = xr.open_dataset(filename)
 print(data)
+press = data['sp']
+eva = data['e']
+
+print(eva)
+
+df_press = press.sel(longitude = np.arange(-25,50,0.5), latitude = np.arange(0,40,0.5), method='nearest')
+
+df_press[1].plot()
