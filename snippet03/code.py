ddb045f2c91ef808c119f7091faf08a34d8524bf
felaris <felixawortwe14@gmail.com>
2021-07-26 12:17:35 -0500

    added snippet03

---
+++ snippet03/code.py
@@ -0,0 +1,8 @@
+import pymannkendall as pmk
+import xarray as xr
+import matplotlib.pyplot as plt 
+import matplotlib as cm
+from pathlib import Path
+
+filename = Path('pressure_evaporation_with_time_and_lon_lat.nc')
+
