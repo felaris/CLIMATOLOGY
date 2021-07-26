6b52c5dba219cd40b87bdcdae82b90d585e6355a
felaris <felixawortwe14@gmail.com>
2021-07-26 10:46:55 -0500

    added a new file trials.py

---
+++ snippet01/trials.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 09:44:17 2021
+
+@author: felaris
+"""
+
+# Importing libraries
+import pandas as pd
+import netCDF4 as nc
+import numpy as np
+import csv
+import glob
+import sys
+from pathlib import Path
+import pymannkendall as pmk
+import xarray as xr
+
+
+filename = Path('pressure_evaporation_with_time_and_lon_lat.nc')
+
+data = xr.open_dataset(filename)
+print(data)
