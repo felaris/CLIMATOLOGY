3774806ca158c5f3ca9cae27e7bffcbe59e6f14a
felaris <felixawortwe14@gmail.com>
2021-07-26 12:41:56 -0500

    added snippet03

--- snippet03/code.py
+++ snippet03/code.py
@@ -3,6 +3,7 @@ import xarray as xr
 import matplotlib.pyplot as plt 
 import matplotlib as cm
 from pathlib import Path
-
+import numpy as np
+import pandas as pd
 filename = Path('pressure_evaporation_with_time_and_lon_lat.nc')
 
